391546cf6fc126c3d927ab1cd289938212a46832
chris crook <ccrook@chriss-MacBook-Pro.local>
2018-06-03 10:38:29 -0600

    add stuff

--- practice/over-utilized-int/attempt2.go
+++ practice/over-utilized-int/attempt2.go
@@ -1,60 +1,69 @@
+/*
+grab interface name: 0/0
+input rate:
+output rate
+input errors:
+crc:
+
+*/
+
 package main
 
 import (
 	"fmt"
 	"log"
 	"regexp"
+	"sort"
 	"strconv"
-	"sync"
 )
 
 func main() {
-	// f, err := ioutil.ReadFile("input/show-interface.txt")
-	// check(err)
+	type ifaceAttrs struct {
+		name      string
+		status    string
+		inRate    int
+		outRate   int
+		inErrors  int
+		crcErrors int
+	}
 
-	// showint := string(f)
-	var wg sync.WaitGroup
-	wg.Add(len(interfaces))
+	var ifaces []ifaceAttrs
 
-	regx := regexp.MustCompile(`(?s)^(\w+\d/\d).*BW (\d+).*input rate (\d+).*output rate (\d+)`)
+	regx := regexp.MustCompile(`(?s).*Ethernet(\d+/\d+) is (\w+).*minute input rate (\d+).*minute output rate (\d+).*  (\d+) input errors, (\d+) CRC,`)
 
 	for _, i := range interfaces {
-		go func(i string) {
-			defer wg.Done()
-			match := regx.FindStringSubmatch(i)
-			if len(match) != 4 {
-				log.Print("No matches")
-			}
-			iface := match[1]
-			bw, err := strconv.ParseFloat(match[2], 1)
-			check(err)
-			bw = bw * 1000
-			input, err := strconv.ParseFloat(match[3], 1)
-			check(err)
-			output, err := strconv.ParseFloat(match[4], 1)
-			check(err)
-
-			in := input / bw * 100
-			out := output / bw
-			//fmt.Println(in, out)
-
-			if in > 85.0 {
-				fmt.Printf("warning - congestion detected on %v: %v\\%\n", iface, in)
-			}
-			if out > 85.0 {
-				fmt.Printf("warning - congestion detected on: %v: %v\\%\n", iface, out)
-			}
-			fmt.Println("here")
-		}(i)
+		match := regx.FindStringSubmatch(i)
+		if match == nil {
+			log.Println("no match")
+			continue
+		}
+		name := match[1]
+		status := match[2]
+		inRate, err := strconv.Atoi(match[3])
+		outRate, err := strconv.Atoi(match[4])
+		inErrors, err := strconv.Atoi(match[5])
+		fmt.Println("INERR ", inErrors)
+		crcErrors, err := strconv.Atoi(match[6])
+		if err != nil {
+			log.Fatal(err)
+		}
+		iface := ifaceAttrs{
+			name,
+			status,
+			inRate,
+			outRate,
+			inErrors,
+			crcErrors,
+		}
+		ifaces = append(ifaces, iface)
 	}
 
-	wg.Wait()
-}
+	sort.Slice(ifaces, func(i, j int) bool { return ifaces[i].crcErrors > ifaces[j].crcErrors })
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
+	for _, intr := range ifaces {
+		fmt.Printf("%+v\n", intr)
 	}
+
 }
 
 var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up 
@@ -75,7 +84,7 @@ var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up
     5 minute output rate 0 bits/sec, 0 packets/sec
        2252 packets input, 135120 bytes, 0 no buffer
        Received 2252 broadcasts, 0 runts, 0 giants, 0 throttles
-       0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
+       55 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
        0 watchdog, 0 multicast, 0 pause input
        0 input packets with dribble condition detected
        2631 packets output, 268395 bytes, 0 underruns
@@ -101,7 +110,7 @@ var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up
     5 minute output rate 850050000 bits/sec, 0 packets/sec
        2252 packets input, 135120 bytes, 0 no buffer
        Received 2252 broadcasts, 0 runts, 0 giants, 0 throttles
-       0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
+       2 input errors, 30 CRC, 0 frame, 0 overrun, 0 ignored
        0 watchdog, 0 multicast, 0 pause input
        0 input packets with dribble condition detected
        2631 packets output, 268395 bytes, 0 underruns
--- practice/over-utilized-int/attempt2.py
+++ practice/over-utilized-int/attempt2.py
@@ -1,63 +1,8 @@
-package main
 
-import (
-	"fmt"
-	"log"
-	"regexp"
-	"strconv"
-	"sync"
-)
+from collections import OrderedDict
+import re
 
-func main() {
-	// f, err := ioutil.ReadFile("input/show-interface.txt")
-	// check(err)
-
-	// showint := string(f)
-	var wg sync.WaitGroup
-	wg.Add(len(interfaces))
-
-	regx := regexp.MustCompile(`(?s)^(\w+\d/\d).*BW (\d+).*input rate (\d+).*output rate (\d+)`)
-
-	for _, i := range interfaces {
-		go func(i string) {
-			defer wg.Done()
-			match := regx.FindStringSubmatch(i)
-			if len(match) != 4 {
-				log.Print("No matches")
-			}
-			iface := match[1]
-			bw, err := strconv.ParseFloat(match[2], 1)
-			check(err)
-			bw = bw * 1000
-			input, err := strconv.ParseFloat(match[3], 1)
-			check(err)
-			output, err := strconv.ParseFloat(match[4], 1)
-			check(err)
-
-			in := input / bw * 100
-			out := output / bw
-			//fmt.Println(in, out)
-
-			if in > 85.0 {
-				fmt.Printf("warning - congestion detected on %v: %v\\%\n", iface, in)
-			}
-			if out > 85.0 {
-				fmt.Printf("warning - congestion detected on: %v: %v\\%\n", iface, out)
-			}
-			fmt.Println("here")
-		}(i)
-	}
-
-	wg.Wait()
-}
-
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up 
+ints = ['''GigabitEthernet0/0 is up, line protocol is up 
     Hardware is 82543 (Livengood), address is 00d0.ffb6.4c00 (bia 00d0.ffb6.4c00)
     Internet address is 10.1.1.3/8
     MTU 1500 bytes, BW 1000000 Kbit, DLY 10 usec, 
@@ -75,15 +20,15 @@ var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up
     5 minute output rate 0 bits/sec, 0 packets/sec
        2252 packets input, 135120 bytes, 0 no buffer
        Received 2252 broadcasts, 0 runts, 0 giants, 0 throttles
-       0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
+       55 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
        0 watchdog, 0 multicast, 0 pause input
        0 input packets with dribble condition detected
        2631 packets output, 268395 bytes, 0 underruns
        0 output errors, 0 collisions, 2 interface resets
        0 babbles, 0 late collision, 0 deferred
        0 lost carrier, 0 no carrier, 0 pause output
-       0 output buffer failures, 0 output buffers swapped out`,
-	`GigabitEthernet0/1 is up, line protocol is up 
+       0 output buffer failures, 0 output buffers swapped out''',
+	'''GigabitEthernet0/1 is up, line protocol is up 
     Hardware is 82543 (Livengood), address is 00d0.ffb6.4c00 (bia 00d0.ffb6.4c00)
     Internet address is 10.1.1.5/8
     MTU 1500 bytes, BW 1000000 Kbit, DLY 10 usec, 
@@ -101,11 +46,18 @@ var interfaces = []string{`GigabitEthernet0/0 is up, line protocol is up
     5 minute output rate 850050000 bits/sec, 0 packets/sec
        2252 packets input, 135120 bytes, 0 no buffer
        Received 2252 broadcasts, 0 runts, 0 giants, 0 throttles
-       0 input errors, 0 CRC, 0 frame, 0 overrun, 0 ignored
+       2 input errors, 30 CRC, 0 frame, 0 overrun, 0 ignored
        0 watchdog, 0 multicast, 0 pause input
        0 input packets with dribble condition detected
        2631 packets output, 268395 bytes, 0 underruns
        0 output errors, 0 collisions, 2 interface resets
        0 babbles, 0 late collision, 0 deferred
        0 lost carrier, 0 no carrier, 0 pause output
-       0 output buffer failures, 0 output buffers swapped out`}
+       0 output buffer failures, 0 output buffers swapped out''']
+
+
+iface = OrderedDict(name="", inRate=0, outRate=0, inError=0, crcError=0) 
+regx = re.compile('.*Ethernet(\d+/\d+).*')
+for i in ints:
+    print re.match('.*Ethernet(\d+/\d+)', i, re.S).group()
+           
